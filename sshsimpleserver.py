9dd9113d6b5d233db5911d5f0393c9ebcb6f395d
lowitty <perfectbeauty@qq.com>
2015-08-20 15:16:06 +0800

    INIT

--- sshsimpleserver.py
+++ sshsimpleserver.py
@@ -4,6 +4,7 @@
 # See LICENSE for details.
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "libs"))
+#INIT
 
 from twisted.cred import portal
 from twisted.conch import avatar, recvline, interfaces as conchinterfaces
